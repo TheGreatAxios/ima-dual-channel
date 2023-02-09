b3d4cdd98f597531e89d4628343b724d56b00a68
TheGreatAxios <sawyer@dirtroad.dev>
2023-02-09 13:20:43 -0800

    Message Listerned Added to Proxy and Target Token

--- contracts/InGameNFT.sol
+++ contracts/InGameNFT.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "./lib/Types.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
@@ -8,21 +9,30 @@ contract InGameNFT is AccessControlEnumerable, ERC721URIStorage {
     
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant ORIGIN_CHAIN_HASH = keccak256(abi.encodePacked("staging-utter-unripe-menkar"));
 
     constructor() ERC721("In Game NFT", "INGAME") {
         _setupRole(MINTER_ROLE, 0xd2AAa00100000000000000000000000000000000);
         _setupRole(BURNER_ROLE, 0xd2AAa00100000000000000000000000000000000);
     }
 
-    function mint(address to, uint256 tokenId) public returns (bool) {
-        require(hasRole(MINTER_ROLE, _msgSender()), "Sender is not a Minter");
-        _mint(to, tokenId);
-        return true;
-    }
-
     function burn(uint256 tokenId) public virtual {
         require(hasRole(BURNER_ROLE, _msgSender()) || _isApprovedOrOwner(_msgSender(), tokenId), "ERC721Burnable: caller is not owner nor approved");
         _burn(tokenId);
+    }
+
+    function postMessage(
+        bytes32 schainHash,
+        address sender,
+        bytes calldata data
+    )
+        external
+    {
+        require(schainHash == ORIGIN_CHAIN_HASH, "Invalid Origin Chain");
+        require(hasRole(MINTER_ROLE, _msgSender()), "Sender is not a Minter");
+        (Types.MintResponse memory res) = abi.decode(data, (Types.MintResponse));
+        _mint(res.to, res.tokenId);
+        _setTokenURI(res.tokenId, res.tokenURI);
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, AccessControlEnumerable) returns (bool) {
--- contracts/NFTProxy.sol
+++ contracts/NFTProxy.sol
@@ -1,17 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract NFTProxy {
+import "./interfaces/IDualChannel721.sol";
+import "./lib/Types.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-    // function postMessage(
-    //     bytes32 schainHash,
-    //     address sender,
-    //     bytes calldata data
-    //     )
-    //     external
-    //     returns (address)
-    // {
-    //     [add in your processing logic]
-    // }
+contract NFTProxy is AccessControlEnumerable {
+
+    bytes32 public constant IMA_ROLE = keccak256("IMA_ROLE");
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE"); 
+
+    event AddClone(bytes32 indexed chainHash, address indexed token, bool indexed isERC721);
+    event MintRequest(bytes32 indexed fromChainHash, address indexed sender, Types.MintRequest request);
+
+    mapping(bytes32 => mapping(IDualChannel721 => bool)) public is721Clone;
+
+    modifier onlyIMA() {
+        require(hasRole(IMA_ROLE, msg.sender), "Missing IMA_ROLE");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(hasRole(OWNER_ROLE, msg.sender), "Missing OWNER_ROLE");
+        _;
+    }
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(OWNER_ROLE, msg.sender);
+        _setupRole(IMA_ROLE, 0xd2AAa00100000000000000000000000000000000);
+    }
+
+    function postMessage(
+        bytes32 schainHash,
+        address sender,
+        bytes calldata data
+    )
+        external
+        onlyIMA
+    {
+        (Types.MintRequest memory req) = abi.decode(data, (Types.MintRequest));
+        require(is721Clone[schainHash][IDualChannel721(req.originToken)], "Not a valid token");
+        IDualChannel721(req.originToken).mint(req);
+        emit MintRequest(schainHash, sender, req);
+    }
+
+    function add721Clone(string memory chainName, IDualChannel721 token) external onlyOwner {
+        bytes32 chainHash = keccak256(abi.encodePacked(chainName));
+        is721Clone[chainHash][token] = true;
+        emit AddClone(chainHash, address(token), true);
+    }
 
 }
